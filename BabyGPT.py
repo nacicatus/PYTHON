8ecb2eff61a86addb0969a310ccc3889f4d0e7b6
AndreaT22 <andrea.toth22@yahoo.com>
2023-05-04 19:54:49 +0200

    corrected

--- BabyGPT.py
+++ BabyGPT.py
@@ -3,5 +3,5 @@ person=input('What is your name?')
 print('Hello!',person)
 print ('Nice to meet you!',person)
 feeling=input('How was your day?')
-print('I\'m happy to hear that it was'+feeling)
+print('I\'m happy to hear that it was',feeling)
 
