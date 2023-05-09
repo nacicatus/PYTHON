7d1dabdb3295c0df5291735cb2fb1e725b92ad55
AndreaT22 <andrea.toth22@yahoo.com>
2023-05-09 20:26:02 +0200

    I learnt some variables today

---
+++ DATAstructures.py
@@ -0,0 +1,36 @@
+# this is a comment
+# print ('Hello')
+# variables
+# These are a few of my favourite things - Sound of Music
+
+Raindrops_on_roses=101 #this is a variable that stores an integer (whole number)
+# str(Raindrops_on_roses) - we converted integar to a string by adding 'str'
+print('I counted ' + str (Raindrops_on_roses) + ' raindrops in the garden in the morning')
+
+Whiskers_on_kittens=8.5 #this is another 'box' and it stores another number (float number - not whole)
+print(Whiskers_on_kittens)
+print ('I saw ' + str (Whiskers_on_kittens) + ' whiskers on each of my kittens')
+
+brown_paper_packages='surprise' # this is a string held by 'pegs'
+print ('I prepared a ' + brown_paper_packages + ' for my grandma') # you don't need to make this a STR cause it's already a string - surprise
+print(brown_paper_packages)
+
+
+# a list variable
+friends_and_family=['Geri', 'Anyu', 'Zoli', 'Apu', 'Kinga']
+friends_and_family.append('Aurelia')
+print(friends_and_family)
+print(friends_and_family[2])
+friends_and_family.insert(3,'Saurabh')
+print(friends_and_family)
+Geri='0429'
+
+# dictionary = key : value - pairs - each of them has an index (sets don't have an index)
+birthdays={'Geri':'0429', 'Anyu':'0522','Zoli':'0914'}
+
+#sets
+#tuples - HWK - check link 
+
+score=100
+score=score+1
+print('your score is ' + str (score))
